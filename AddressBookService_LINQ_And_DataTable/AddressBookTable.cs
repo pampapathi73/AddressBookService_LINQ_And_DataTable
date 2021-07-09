cc9564f99ab2ee0141a22bb119cb11aafa07779c
pampapathi73 <pampapathik32@gmail.com>
2021-07-09 11:38:13 +0530

    [pampapathi] Added: CreateAddressBookWithAttributes

--- AddressBookService_LINQ_And_DataTable/AddressBookTable.cs
+++ AddressBookService_LINQ_And_DataTable/AddressBookTable.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
 
 namespace AddressBookService_LINQ_And_DataTable
 {
     class AddressBookTable
     {
+        public void CreateAddressBookDataTable()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("FirstName", typeof(string));
+            table.Columns.Add("LastName", typeof(string));
+            table.Columns.Add("Address", typeof(string));
+            table.Columns.Add("City", typeof(string));
+            table.Columns.Add("State", typeof(string));
+            table.Columns.Add("Zip", typeof(int));
+            table.Columns.Add("PhoneNumber", typeof(long));
+            table.Columns.Add("Email", typeof(string));
+        }
     }
 }
